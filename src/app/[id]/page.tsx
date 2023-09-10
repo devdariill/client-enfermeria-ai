596fb4c0fec0407d10a0b6ca32463d2237552ae4
name <you@example.com>
2023-09-10 06:23:03 -0500

    _

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,11 +1,15 @@
+import api from '@/app/api'
 import Card from '../components/Card'
+// import data from '../mock/data.json'
+const data = api.data.people()
+console.log('🚀 ~ file: page.tsx:6 ~ data:', data)
 
 export default function Page ({ params }: { params: { id: string } }) {
-  console.log(params)
+  const nursingRecords = data.filter((item) => item.patient_id === +params.id)[0].nursing_records
   return (
     <div className='h-full'>
-      My Post: {params.id}
-      <Card />
+      {/* My Post: {params.id} */}
+      <Card nursingRecords={nursingRecords} />
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import data from '@/app/mock/data.json'
+import api from '@/app/api'
 import { ListOfPeople } from './components/listOfPeople'
-import { People } from './types'
+
+const data = api.data.people()
 
 export default async function Home () {
   return (
     <main className='mx-auto px-4 max-w-[1500px] pb-24'>
-      <ListOfPeople people={data as unknown as People[]} />
+      <ListOfPeople people={data} />
     </main>
   )
 }
