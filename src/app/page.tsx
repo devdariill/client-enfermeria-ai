8d7cbd9fbdfa0b4fabb9bb40d5b833889a4df471
name <gowluonade@gmail.com>
2023-10-10 10:57:49 -0500

    _NODE_ENV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import { ButtonLink } from '../components/button-link'
 export const dynamic = 'force-dynamic'
 
 export default async function Home () {
+  console.log(process.env.NODE_ENV)
   return (
     <section className='mx-auto px-4 gap-5 flex flex-col py-10 w-full'>
+      <div>{process.env.NODE_ENV}</div>
       <ButtonLink href='/list'>Enfermeria</ButtonLink>
       <ButtonLink href='/tercero'>Terceros</ButtonLink>
       <ButtonLink href='/list/charts'>Informes</ButtonLink>
