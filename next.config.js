1cb77e45a8744784166f2c6b5656b928051725a5
name <gowluonade@gmail.com>
2023-10-17 19:22:57 -0500

    _

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
   experimental: {
     serverActions: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
   }
-  // eslint: {
-  //   ignoreDuringBuilds: true
-  // },
-  // typescript: {
-  //   ignoreBuildErrors: true
-  // }
 }
 
 module.exports = nextConfig
